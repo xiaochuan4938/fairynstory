504f9607438f8fceaf0edebc21191179d07580e7
xiaochuanyao <xyao@mobiwork.com>
2024-06-27 10:02:29 -0400

    update

---
+++ Little Read Hem.rb
@@ -0,0 +1,164 @@
+
+once upon a time the animals in a farm
+were left with no food
+the little red hen decided to wander
+around the field to look for something
+to eat
+
+she first went next to the cow
+will you come with me around the field to find something to eat
+no i won't it's too hot i can't be bothered to walk
+
+she then went next to the pig
+will you help me find food
+no i can't calm
+it's too hot and i can't be bothered to move 
+later then went next to the dog
+will you help me find food no i can't
+it's too hot and i can't walk when it's hot
+
+and in the end went next to the duck will you come with me to find something to eat 
+no i can't it's too hot i can't get out of the water
+
+
+when nobody bothered to come with her
+the hen decided to leave the farm on her
+own
+as she walked she found some wheat
+grains on the ground
+
+
+she was very happy
+she returned to the farm
+she decided to plant the wheat grains
+she thought that her friends would help her
+
+cow look i found wheat grains would you like to plant them with me
+no i already told you it's too hot
+the hen went over to the pig
+big look i found wheatgrains would you
+like to plant them with me
+no i can't the weather is way too hot
+for this
+she then went next to the dog
+dog look i found weak grains would you
+like to plant them with me
+no it's way too hot
+and at last she went next to the duck
+hey doc i found some wheatgrains would
+you like to plant them with me
+not okay i can't leave the water in this
+heat
+well i'll plant them myself
+when she saw that no one wanted to help
+her
+she decided to plant them herself
+weeks had gone by
+the rainy days had begun the seeds had
+sprouted
+but all the wild grass in the garden
+needed some cleaning
+who's going to help me clean the grass
+it's too muddy now i can't help you i'm
+not up to it i won't leave my spot
+i'll get dirty can't do it don't feel
+good today i can't help
+in that case i'll do it on my own
+the little red hen began to clean the
+wild grass amongst the sprouts
+not long after the wheat began to grow
+it was now time to harvest the ripe
+wheat
+the hen went next to her friends and
+asked if they would help her harvest the
+crop hey cow buddy
+wheat has grown will you help me harvest
+the crop no i can't
+hey pig guess what the wheat has grown
+will you help me harvest the crop
+no i can't i won't
+hey my buddy dog the wheat has grown
+will you come and help me harvest the
+crop who me of course not hey ducky
+ducky the wheat is grown will you help
+me harvest the crop of course i can't
+okay i'll do it on my own
+the little red hen worked till night
+time
+she harvested the wheat kernels one by
+one all by herself
+
+it was now time to turn the wheat to
+flour
+off she went to ask for help from her
+friends hey guys
+we must grind the wheat to make flour
+who would like to help me
+i can't help it's time for me to give
+milk i can't move from my sport
+
+i can't help peter it's snap time for me
+i can't help at all
+can't help me to get into the water and
+cool down
+the hen ground the wheat in the mill and
+turned it into flour now let's make some
+delicious bread
+the hen went next to her friends and
+wanted to give them one last chance
+cow i'm going to make bread would you
+like to help me nope i can't i'm in no
+situation to work
+what about you pig
+not today i'm too tired to have today
+no no i can't no i can't and besides i
+don't know how to make bread
+this time the hen was very angry
+all by herself she went to the kitchen
+first she made bread with the flour she
+had grounded
+then she gave it a form
+and at last
+put it in the oven and waited for it to
+bake
+after the amazing smell of the bread had
+spread
+she took it out of the oven
+went out to the garden and sat on the
+table
+later
+called out to her friends
+hey guys the bread is ready who would
+like to eat it with me
+seeing the amazing bread in front of the
+hen in a flash they all went next to her
+
+no i want
+oh me too
+right when i'm so
+hungry
+great timing hen
+hey me too hen i love bread come on
+let's eat
+no i can't i can't
+i did everything on my own only i
+deserve to eat it all on my own
+
+with great appetite the hen began to eat
+her bread
+but couldn't handle the fact that her
+friends were so hungry
+from now on if you promise to help
+i will share my bread with you
+all the farmyard animals were ashamed
+and sorry
+they knew she was right
+we promise you
+this
+the hen knew her friends learnt a good
+lesson
+so she shared her bread with them
+an amazing appetite they were now so
+happy
+with a full tummy
+
---
+++ The Gingerbread Man.rb
@@ -0,0 +1,84 @@
+
+The Gingerbread Man
+
+once upon a time there lived an old couple in an old small shack next to a
+forest they were living a happy and peaceful life their only regret was not having a
+child of their own one day when the old lady was making cookie dough in the kitchen her husband
+came in.
+darling what are you cooking today 
+oh my darling i'm baking a gingerbread
+man today the old lady knead the dough and cut a
+gingerbread man shape after putting it in the oven she sat down and started to wait for the
+gingerbread man to bake when she could smell the delicious
+cookie all around the air in the kitchen she put on her oven gloves and took the gingerbread man out
+now it was time to decorate it she made eyes out of raisins
+and a cute nose using candy and then she used some cream to make his
+hair and clothes and lastly she used cherries to make some buttons for him
+she had to look at her masterpiece and said my gingerbread man looks beautiful
+but i feel like something is missing the old lady looked at him again and oh
+his mouth i forgot to make his mouth she drew a mouth on the gingerbread man's face with
+the cream oh yes now you are complete my gingerbread man
+at that moment something unexpected happened thank you but but how can it be you are talking
+gingerbread man suddenly stood up and started running yeah and i can also run
+the gingerbread man jumped from the kitchen bench to the chair then to the ground and started running fast to the
+kitchen door to the garden 
+come back the old lady yelled
+ the gingerbread man began talking whilst
+he was running yeah run run as fast as you can but nobody can catch me because i'm the
+gingerbread man the old lady got out to the garden and started running after the gingerbread
+man the man looked out the window and saw his wife running and yelled hey
+where are you running the old lady answered to her husband whilst running
+my gingerbread man ran away i'm trying to catch him
+the old man was speechless the old lady
+ran but the gingerbread man was so fast that it was impossible to catch him after a short while the gingerbread man
+came across a ranch a grazing cow noticed him
+ah what a nice cookie i should cut and eat him the cow also began to run after the
+gingerbread man wait don't run i'm gonna eat ya yeah run run as fast as you can
+and old ladies also trying to catch me but nobody can because i'm the gingerbread man
+the old lady and the cow were running after the gingerbread man
+and at this time a pig noticed the gingerbread man 
+a gingerbread man true to my tastebuds wait and i will catch you
+the gingerbread man answered the pig whilst he was running yeah run run as
+fast as you can an old lady and a cow are also trying to catch me but nobody care because i'm the
+gingerbread man while they were running the gingerbread man in front the old lady cow and the
+pink behind him a chicken noticed the gingerbread man while looking for some food
+that has to be my lunch so the chicken tagged along rum as much
+as you want gingerbread man i'm gonna cut you yeah run run as fast as you can an old
+lady a cow and a pig could not catch me neither can you nobody can because i'm
+the gingerbread man the gingerbread man in front the old lady cow the pig and the chicken they
+all continued to run but the gingerbread man was getting more and more further ahead from the others
+the gingerbread man was so happy and very proud of himself i'm the brightest
+and the fastest gingerbread man in the world yes that's me
+nobody can catch me because i'm the gingerbread man
+when he looked ahead soon the tutor bad man saw that he was coming across a river
+and he stopped because he knew that water could make him melt away
+on oh what now the old lady cow pig and the chicken
+were close now right at that moment a shifty fox appeared from behind a tree
+i know how to swim if you want i can help you the gingerbread man thought about it
+what if you eat me you don't have to worry i don't want to eat you i just want to help you get
+across the gingerbread man trusted the shifty fox and jumped on his tail holding on as tight as he could the fox
+jumped in the river and began to swim meanwhile the old lady cow pig and the
+chicken came to the edge of the river and saw the gingerbread man crossing the river on the back of a fox
+helplessly they watched them go knowing they could not catch him anymore
+the river began to get deeper and the waters start to rise hey fox keep your
+tail up i almost got wet up on my back it's safer
+the gingerbread man hopped on the fox's back they swam for a while but as the water got deeper the fox's back began to
+sink in the water i'm afraid that you'll get wet why don't
+you jump on my head where it's a bit higher the gingerbread man climbed up on his
+head the fox continued to pursue his plan and dipped his head down in the water
+the water has risen too much why don't you get on my nose it's higher
+so the gingerbread man got on top of his nose right when they were about to reach the shore the fox dipped his nose
+flipping the gingerbread man into the air and opened his mouth the gingerbread man was going to fall into his mouth and
+the fox was going to eat him but it didn't work while the gingerbread
+man was in the air a crow flying right above them caught the gingerbread man with his beak
+the fox just stood there looking with his mouth open the gingerbread man waited for the crow
+to fly a little further and asked do crows eat ginger cookies
+yep when the crow opened his beak to speak the gingerbread man fell down and began to run as fast as he could
+yeah run run as fast as you can an old lady a cow a pig a chicken a fox and a crow
+also trying to catch me but nobody can because i'm the gingerbread man
+the gingerbread man kept on running and did not stop 
+if you see a gingerbread man pass you by running do not try to catch him
+because he is the gingerbread man and nobody can dare catch him.
+
+
+
